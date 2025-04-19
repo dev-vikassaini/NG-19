4842b9a3e2b4d69d8d3104767bb9a759b0435e2d
Vikas Kumar <Vikas.Kumar@adek.gov.ae>
2025-04-19 13:48:13 +0530

    Add new task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,18 +3,19 @@ import { HeaderComponent } from './layout/header/header.component';
 import { UserComponent } from "./components/user/user.component";
 import { DUMMY_USERS } from './constants/dummy-users';
 import { UserSignalComponent } from './components/user-signal/user-signal.component';
-import { TaskComponent } from "./components/task/task.component";
+import { NgFor, NgIf } from '@angular/common';
+import { TasksComponent } from './components/tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent, UserSignalComponent, TaskComponent],
+  imports: [HeaderComponent, UserComponent, UserSignalComponent, TasksComponent, NgFor, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   users = DUMMY_USERS;
-  selectedUserId = 'u1';
+  selectedUserId?: string = '';
 
   onSelectUser(id: string) {
     this.selectedUserId = id;
